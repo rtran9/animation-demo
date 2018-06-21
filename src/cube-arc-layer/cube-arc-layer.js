6080d23054a481785d0d475137500419ab5eb4cb
robert <rtran@mit.edu>
2018-06-20 21:48:13 -0400

    cube arc layer

---
+++ src/cube-arc-layer/cube-arc-layer.js
@@ -0,0 +1,244 @@
+import {COORDINATE_SYSTEM, Layer} from 'deck.gl';
+import {GL, Model, Geometry} from 'luma.gl';
+import {Matrix4} from 'math.gl';
+
+import vs from './cube-vertex.glsl';
+import fs from './cube-fragment.glsl';
+
+const DEFAULT_COLOR = [0, 0, 0, 255];
+
+const defaultProps = {
+  getSourcePosition: x => x.sourcePosition,
+  getTargetPosition: x => x.targetPosition,
+  getSourceColor: x => x.color || DEFAULT_COLOR,
+  getTargetColor: x => x.color || DEFAULT_COLOR,
+  getStrokeWidth: 1
+};
+
+export default class CubeArcLayer extends Layer {
+  constructor(props) {
+    let overrideProps = null;
+    if (Number.isFinite(props.strokeWidth)) {
+      overrideProps = {
+        getStrokeWidth: props.strokeWidth
+      };
+    }
+    super(props, overrideProps);
+  }
+
+  getShaders() {
+    return {
+			vs,
+			fs,
+			modules: ['picking']
+		}; // 'project' module added by default.
+  }
+
+  initializeState() {
+    const attributeManager = this.getAttributeManager();
+    const {gl} = this.context;
+
+    /* eslint-disable max-len */
+    attributeManager.addInstanced({
+      instancePositions: {
+        size: 4,
+        transition: true,
+        accessor: ['getSourcePosition', 'getTargetPosition'],
+        update: this.calculateInstancePositions
+      },
+      instanceSourceColors: {
+        size: 4,
+        type: GL.UNSIGNED_BYTE,
+        transition: true,
+        accessor: 'getSourceColor',
+        defaultValue: DEFAULT_COLOR
+      },
+      instanceTargetColors: {
+        size: 4,
+        type: GL.UNSIGNED_BYTE,
+        transition: true,
+        accessor: 'getTargetColor',
+        defaultValue: DEFAULT_COLOR
+      },
+      instanceWidths: {
+        size: 1,
+        transition: true,
+        accessor: 'getStrokeWidth',
+        defaultValue: 1
+      }
+    });
+
+    this.setState({
+      model: this._getModel(gl)
+    });
+
+    loadTextures(gl, {
+      // https://www.iconfinder.com/icons/2714753/2107_auto_automobile_avtovaz_car_lada_vehicle_icon
+      urls: ['car.svg']
+    }).then(textures => {
+      this.state.model.setUniforms({
+        uSampler: textures[0]
+      })
+    });
+
+    /* eslint-enable max-len */
+  }
+
+  updateAttribute({props, oldProps, changeFlags}) {
+    const attributeManager = this.getAttributeManager();
+    attributeManager.invalidateAll();
+
+    if (props.fp64 && props.coordinateSystem === COORDINATE_SYSTEM.LNGLAT) {
+      attributeManager.addInstanced({
+        instancePositions64Low: {
+          size: 4,
+          accessor: ['getSourcePosition', 'getTargetPosition'],
+          update: this.calculateInstancePositions64Low
+        }
+      });
+    } else {
+      attributeManager.remove(['instancePositions64Low']);
+    }
+  }
+
+  updateState({props, oldProps, changeFlags}) {
+    super.updateState({props, oldProps, changeFlags});
+
+		const {gl} = this.context;
+		if (this.state.model) {
+			this.state.model.delete();
+		}
+		this.setState({
+			model: this._getModel(gl)
+		});
+
+    this.state.model.setUniforms({
+			currentTime: props.currentTime
+		});
+
+    this.updateAttribute({props, oldProps, changeFlags});
+  }
+
+  _getModel(gl) {
+		const model = new Model(
+      gl,
+      Object.assign({}, this.getShaders(), {
+        id: this.props.id,
+        geometry: new Geometry({
+          drawMode: GL.TRIANGLE_STRIP,
+          attributes: {
+            positions: new Float32Array([
+              -1, -1,  1,
+               1, -1,  1,
+               1,  1,  1,
+              -1,  1,  1,
+
+              -1, -1, -1,
+              -1,  1, -1,
+               1,  1, -1,
+               1, -1, -1,
+
+              -1,  1, -1,
+              -1,  1,  1,
+               1,  1,  1,
+               1,  1, -1,
+
+              -1, -1, -1,
+               1, -1, -1,
+               1, -1,  1,
+              -1, -1,  1,
+
+               1, -1, -1,
+               1,  1, -1,
+               1,  1,  1,
+               1, -1,  1,
+
+              -1, -1, -1,
+              -1, -1,  1,
+              -1,  1,  1,
+              -1,  1, -1
+						]),
+
+            colors: {
+              size: 4,
+              value: new Float32Array([
+                1, 0, 0, 1,
+                1, 0, 0, 1,
+                1, 0, 0, 1,
+                1, 0, 0, 1,
+
+                1, 1, 0, 1,
+                1, 1, 0, 1,
+                1, 1, 0, 1,
+                1, 1, 0, 1,
+
+                0, 1, 0, 1,
+                0, 1, 0, 1,
+                0, 1, 0, 1,
+                0, 1, 0, 1,
+
+                1, 0.5, 0.5, 1,
+                1, 0.5, 0.5, 1,
+                1, 0.5, 0.5, 1,
+                1, 0.5, 0.5, 1,
+
+                1, 0, 1, 1,
+                1, 0, 1, 1,
+                1, 0, 1, 1,
+                1, 0, 1, 1,
+
+                0, 0, 1, 1,
+                0, 0, 1, 1,
+                0, 0, 1, 1,
+                0, 0, 1, 1
+              ])
+            },
+
+            indices: new Uint16Array([
+              0, 1, 2, 0, 2, 3,
+              4, 5, 6, 4, 6, 7,
+              8, 9, 10, 8, 10, 11,
+              12, 13, 14, 12, 14, 15,
+              16, 17, 18, 16, 18, 19,
+              20, 21, 22, 20, 22, 23
+            ])
+          }
+      	}),
+      	isInstanced: true,
+      	shaderCache: this.context.shaderCache
+  		})
+		);
+
+    const scaleFactor = 0.03;
+    const scaler = new Matrix4().scale([scaleFactor, scaleFactor, scaleFactor]);
+
+    model.setUniforms({
+      uSMatrix: scaler
+    });
+
+    return model;
+  }
+
+  calculateInstancePositions(attribute) {
+    const {data, getSourcePosition, getTargetPosition} = this.props;
+    const {value, size} = attribute;
+
+    let i = 0;
+    for (let j = 0; j < data.length; j++) {
+			const object = data[j];
+
+      const sourcePosition = getSourcePosition(object);
+      const targetPosition = getTargetPosition(object);
+
+			value[i + 0] = sourcePosition[0];
+      value[i + 1] = sourcePosition[1];
+      value[i + 2] = targetPosition[0];
+      value[i + 3] = targetPosition[1];
+
+      i += size;
+    }
+  }
+}
+
+CubeArcLayer.layerName = 'CubeArcLayer';
+CubeArcLayer.defaultProps = defaultProps;
---
+++ src/cube-arc-layer/cube-vertex.glsl.js
@@ -0,0 +1,72 @@
+export default `\
+#define SHADER_NAME cube-arc-layer-vertex-shader
+
+attribute vec4 colors;
+attribute vec3 positions;
+
+attribute vec4 instanceSourceColors;
+attribute vec4 instanceTargetColors;
+attribute vec4 instancePositions;
+attribute vec3 instancePickingColors;
+attribute float instanceWidths;
+
+uniform mat4 uSMatrix;
+uniform float currentTime;
+
+varying vec4 vColor;
+
+float paraboloid(vec2 source, vec2 target, float ratio) {
+  vec2 x = mix(source, target, ratio);
+  vec2 center = mix(source, target, 0.5);
+
+  float dSourceCenter = distance(source, center);
+  float dXCenter = distance(x, center);
+  return (dSourceCenter + dXCenter) * (dSourceCenter - dXCenter);
+}
+
+vec3 getPos(vec2 source, vec2 target, float segmentRatio) {
+  float vertex_height = paraboloid(source, target, segmentRatio);
+
+  return vec3(
+    mix(source, target, segmentRatio),
+    sqrt(max(0.0, vertex_height))
+  );
+}
+
+void main(void) {
+	vec2 source = project_position(instancePositions.xy);
+  vec2 target = project_position(instancePositions.zw);
+
+	float vAlpha = currentTime / 10.0;
+
+	vec3 currPos = getPos(source, target, vAlpha + 0.01);
+	vec3 nextPos = getPos(source, target, vAlpha - 0.01);
+  vec4 curr = project_to_clipspace(vec4(currPos, 1.0));
+	vec4 next = project_to_clipspace(vec4(nextPos, 1.0));
+
+	mat4 translationMatrix = mat4(
+		vec4(1.0, 0, 0, 0),
+		vec4(0, 1.0, 0, 0),
+		vec4(0, 0, 1.0, 0),
+		curr
+	);
+
+  vec3 lookat = next.xyz;
+  vec3 pos = curr.xyz;
+  vec3 upVector = vec3(0.0, 1.0, 0.0);
+
+  vec3 orientZ = normalize(next.xyz - curr.xyz);
+  vec3 orientX = normalize(cross(upVector, orientZ));
+  vec3 orientY = cross(orientZ, orientX);
+
+  mat4 orientationMatrix = mat4(
+    vec4(orientX, 1.0),
+    vec4(orientY, 1.0),
+    vec4(orientZ, 1.0),
+    vec4(0, 0, 0, 1.0)
+  );
+
+	gl_Position = translationMatrix * orientationMatrix * uSMatrix * vec4(positions, 1.0);
+  vColor = colors;
+}
+`;
